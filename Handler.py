Fixed perspective rotation. Added a plane.

Handler.py
@@ -1,18 +1,20 @@
+from typing import TypedDict
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import QModelIndex
 import numpy as np
 from numpy.typing import NDArray
 from typing import Union
 from OpenGL.GL import *
+from utils import Sphere_utils
 
 
 class PushButtonEvent:
-    def __init__(self, window):
+    def __init__(self, window) -> None:
         self.window = window
 
     def switch_projection_mode(self) -> None:
-        current_mode = self.window.OpenGLWindow.IS_PERSPECTIVE
-        if current_mode:
+        CurrentMode = self.window.OpenGLWindow.IS_PERSPECTIVE
+        if CurrentMode:
             self.window.OpenGLWindow.IS_PERSPECTIVE = False
             print(f"IS_PERSPECTIVE={self.window.OpenGLWindow.IS_PERSPECTIVE}")
         else:
@@ -20,63 +22,136 @@ class PushButtonEvent:
             print(f"IS_PERSPECTIVE={self.window.OpenGLWindow.IS_PERSPECTIVE}")
         self.window.OpenGLWindow.repaint()
 
+    def add_or_delelte_coordinate_axis(self):
+        Current: bool = self.window.OpenGLWindow.COORDINATE_AXIS
+        ObjectList: list = self.window.OpenGLWindow.Graphics
+        if Current:
+            Axis_Index: int = self.window.OpenGLWindow.Graphics.index(
+                (
+                    self.window.OpenGLWindow.AxisVao,
+                    self.window.OpenGLWindow.AxisVbo,
+                    self.window.OpenGLWindow.AxisEbo,
+                    6,
+                    GL_LINES,
+                )
+            )
+            (
+                vao,
+                vbo,
+                ebo,
+                _,
+                _,
+            ) = ObjectList[Axis_Index]
+            self.window.OpenGLWindow.delete_single_object(vao, vbo, ebo)
+            del ObjectList[Axis_Index]
+            self.window.OpenGLWindow.COORDINATE_AXIS = False
+        else:
+            self.window.OpenGLWindow.draw_coordinates()
+            self.window.OpenGLWindow.COORDINATE_AXIS = True
+
+        self.window.OpenGLWindow.update()
+
+    def add_or_delelte_plane(self):
+        Current: bool = self.window.OpenGLWindow.PLANE
+        ObjectList: list = self.window.OpenGLWindow.Graphics
+        if Current:
+            Plane_Index: int = self.window.OpenGLWindow.Graphics.index(
+                (
+                    self.window.OpenGLWindow.PlaneVao,
+                    self.window.OpenGLWindow.PlaneVbo,
+                    self.window.OpenGLWindow.PlaneEbo,
+                    24,
+                    GL_QUADS,
+                )
+            )
+            (
+                vao,
+                vbo,
+                ebo,
+                _,
+                _,
+            ) = ObjectList[Plane_Index]
+            self.window.OpenGLWindow.delete_single_object(vao, vbo, ebo)
+            del ObjectList[Plane_Index]
+            self.window.OpenGLWindow.PLANE = False
+        else:
+            self.window.OpenGLWindow.draw_plane()
+            self.window.OpenGLWindow.PLANE = True
+
+        self.window.OpenGLWindow.update()
+
     def add_object(self) -> None:
         dialog = AddObjectDialog(self.window)
 
         if dialog.exec() == QDialog.DialogCode.Accepted:
-            data: dict[str, Union[str, int, float]] = dialog.get_data()
+            data: ObjectDataType = dialog.get_data()
             print(data)
 
         Type: str = data["Type"]
-        Side_Length: int = data["Side_Length"]
-        X_Coordinate: int = data["X_Coordinate"]
-        Y_Coordinate: int = data["Y_Coordinate"]
-        Z_Coordinate: int = data["Z_Coordinate"]
+        Side_Length: float = data["Side_Length"]
+        X_Coordinate: float = data["X_Coordinate"]
+        Y_Coordinate: float = data["Y_Coordinate"]
+        Z_Coordinate: float = data["Z_Coordinate"]
         R_v: float = data["R"]
         G_v: float = data["G"]
         B_v: float = data["B"]
         A_v: float = data["A"]
 
-        if Type == "Equilateral triangle":
-            self.add_triangle(
-                Side_Length,
-                X_Coordinate,
-                Y_Coordinate,
-                Z_Coordinate,
-                R_v,
-                G_v,
-                B_v,
-                A_v,
-            )
-        elif Type == "Square":
-            self.add_square(
-                Side_Length,
-                X_Coordinate,
-                Y_Coordinate,
-                Z_Coordinate,
-                R_v,
-                G_v,
-                B_v,
-                A_v,
-            )
-        elif Type == "Cube":
-            self.add_cube(
-                Side_Length,
-                X_Coordinate,
-                Y_Coordinate,
-                Z_Coordinate,
-                R_v,
-                G_v,
-                B_v,
-                A_v,
-            )
+        match Type:
+            case "Equilateral triangle":
+                self.add_triangle(
+                    Side_Length,
+                    X_Coordinate,
+                    Y_Coordinate,
+                    Z_Coordinate,
+                    R_v,
+                    G_v,
+                    B_v,
+                    A_v,
+                )
+
+            case "Square":
+                self.add_square(
+                    Side_Length,
+                    X_Coordinate,
+                    Y_Coordinate,
+                    Z_Coordinate,
+                    R_v,
+                    G_v,
+                    B_v,
+                    A_v,
+                )
+
+            case "Cube":
+                self.add_cube(
+                    Side_Length,
+                    X_Coordinate,
+                    Y_Coordinate,
+                    Z_Coordinate,
+                    R_v,
+                    G_v,
+                    B_v,
+                    A_v,
+                )
+            
+            case "Sphere":
+                self.add_sphere(
+                    Side_Length,
+                    X_Coordinate,
+                    Y_Coordinate,
+                    Z_Coordinate,
+                    R_v,
+                    G_v,
+                    B_v,
+                    A_v,
+                )
 
     def add_triangle(
         self,
-        Side_Length: int,
-        X_Coordinate: int,
-        Y_Coordinate: int,
-        Z_Coordinate: int,
+        Side_Length: float,
+        X_Coordinate: float,
+        Y_Coordinate: float,
+        Z_Coordinate: float,
         R_v: float,
         G_v: float,
         B_v: float,
@@ -92,7 +167,7 @@ class PushButtonEvent:
         Indices: NDArray[np.uint32] = np.array([0, 1, 2], dtype=np.uint32)
         # fmt: on
 
-        ObjectData: dict[str, Union[int, NDArray[np.float32]]] = {
+        ObjectData: dict[str, Union[int, NDArray[np.float32], NDArray[np.uint32]]] = {
             "Type": GL_TRIANGLES,
             "Vertices": Vertices,
             "Indices": Indices,
@@ -102,15 +177,15 @@ class PushButtonEvent:
 
     def add_square(
         self,
-        Side_Length: int,
-        X_Coordinate: int,
-        Y_Coordinate: int,
-        Z_Coordinate: int,
+        Side_Length: float,
+        X_Coordinate: float,
+        Y_Coordinate: float,
+        Z_Coordinate: float,
         R_v: float,
         G_v: float,
         B_v: float,
         A_v: float,
-    ):
+    ) -> None:
         # fmt: off
         Vertices: NDArray[np.float32] = np.array([
             X_Coordinate - (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate, R_v, G_v, B_v, A_v,
@@ -122,7 +197,7 @@ class PushButtonEvent:
         Indices: NDArray[np.uint32] = np.array([0, 1, 2, 3], dtype=np.uint32)
         # fmt: on
 
-        ObjectData: dict[str, Union[int, NDArray[np.float32]]] = {
+        ObjectData: dict[str, Union[int, NDArray[np.float32], NDArray[np.uint32]]] = {
             "Type": GL_QUADS,
             "Vertices": Vertices,
             "Indices": Indices,
@@ -132,10 +207,10 @@ class PushButtonEvent:
 
     def add_cube(
         self,
-        Side_Length: int,
-        X_Coordinate: int,
-        Y_Coordinate: int,
-        Z_Coordinate: int,
+        Side_Length: float,
+        X_Coordinate: float,
+        Y_Coordinate: float,
+        Z_Coordinate: float,
         R_v: float,
         G_v: float,
         B_v: float,
@@ -150,14 +225,14 @@ class PushButtonEvent:
         #  v3------v2
         # fmt: off
         Vertices: NDArray[np.float32] = np.array([
-            X_Coordinate - (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate - (Side_Length / 2), R_v, G_v, B_v, A_v,   # v0
-            X_Coordinate + (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate - (Side_Length / 2), R_v, G_v, B_v, A_v,   # v1
-            X_Coordinate + (Side_Length / 2), Y_Coordinate - (Side_Length / 2), Z_Coordinate - (Side_Length / 2), R_v, G_v, B_v, A_v,   # v2
-            X_Coordinate - (Side_Length / 2), Y_Coordinate - (Side_Length / 2), Z_Coordinate - (Side_Length / 2), R_v, G_v, B_v, A_v,   # v3
-            X_Coordinate - (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate + (Side_Length / 2), R_v, G_v, B_v, A_v,   # v4
-            X_Coordinate + (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate + (Side_Length / 2), R_v, G_v, B_v, A_v,   # v5
-            X_Coordinate + (Side_Length / 2), Y_Coordinate - (Side_Length / 2), Z_Coordinate + (Side_Length / 2), R_v, G_v, B_v, A_v,   # v6
-            X_Coordinate - (Side_Length / 2), Y_Coordinate - (Side_Length / 2), Z_Coordinate + (Side_Length / 2), R_v, G_v, B_v, A_v,   # v7
+            X_Coordinate - (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate + (Side_Length / 2), R_v, G_v, B_v, A_v,   # v0
+            X_Coordinate + (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate + (Side_Length / 2), R_v, G_v, B_v, A_v,   # v1
+            X_Coordinate + (Side_Length / 2), Y_Coordinate - (Side_Length / 2), Z_Coordinate + (Side_Length / 2), R_v, G_v, B_v, A_v,   # v2
+            X_Coordinate - (Side_Length / 2), Y_Coordinate - (Side_Length / 2), Z_Coordinate + (Side_Length / 2), R_v, G_v, B_v, A_v,   # v3
+            X_Coordinate - (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate - (Side_Length / 2), R_v, G_v, B_v, A_v,   # v4
+            X_Coordinate + (Side_Length / 2), Y_Coordinate + (Side_Length / 2), Z_Coordinate - (Side_Length / 2), R_v, G_v, B_v, A_v,   # v5
+            X_Coordinate + (Side_Length / 2), Y_Coordinate - (Side_Length / 2), Z_Coordinate - (Side_Length / 2), R_v, G_v, B_v, A_v,   # v6
+            X_Coordinate - (Side_Length / 2), Y_Coordinate - (Side_Length / 2), Z_Coordinate - (Side_Length / 2), R_v, G_v, B_v, A_v,   # v7
         ], dtype=np.float32)
         Indices: NDArray[np.uint32] = np.array([
             0, 1, 2, 3, # v0-v1-v2-v3
@@ -168,8 +243,41 @@ class PushButtonEvent:
             4, 0, 3, 7  # v4-v0-v3-v7
         ], dtype=np.uint32)
         # fmt: on
-        ObjectData: dict[str, Union[int, NDArray[np.float32]]] = {
+        ObjectData: dict[str, Union[int, NDArray[np.float32], NDArray[np.uint32]]] = {
             "Type": GL_QUADS,
+            "Vertices": Vertices,
+            "Indices": Indices,
+        }
+
+        self.window.OpenGLWindow.analysis_data(ObjectData)
+
+    def add_sphere(
+        self,
+        Radius: float,
+        X_Coordinate: float,
+        Y_Coordinate: float,
+        Z_Coordinate: float,
+        R_v: float,
+        G_v: float,
+        B_v: float,
+        A_v: float,
+        Rings: int = 1000,
+        Sectors: int = 1000,
+    ) -> None:
+        Vertices, Indices = Sphere_utils.generate_sphere(
+            Radius,
+            X_Coordinate,
+            Y_Coordinate,
+            Z_Coordinate,
+            Rings,
+            Sectors,
+            R_v,
+            G_v,
+            B_v,
+            A_v,
+        )
+        ObjectData: dict[str, Union[int, NDArray[np.float32], NDArray[np.uint32]]] = {
+            "Type": GL_TRIANGLES,
             "Vertices": Vertices,
             "Indices": Indices,
         }
@@ -183,16 +291,28 @@ class PushButtonEvent:
         self.window.update()
 
 
+class ObjectDataType(TypedDict):
+    Type: str
+    Side_Length: float
+    X_Coordinate: float
+    Y_Coordinate: float
+    Z_Coordinate: float
+    R: float
+    G: float
+    B: float
+    A: float
+
+
 class AddObjectDialog(QDialog):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.setWindowTitle("Add Object")
         self.resize(600, 500)
-        self.r = 0
-        self.g = 0
-        self.b = 0
-        self.a = 0
+        self.r: float = 0.0
+        self.g: float = 0.0
+        self.b: float = 0.0
+        self.a: float = 0.0
         self.init_ui()
 
     def init_ui(self):
@@ -201,7 +321,7 @@ class AddObjectDialog(QDialog):
 
         self.SelectType = QComboBox()
         # fmt: off
-        self.SelectType.addItems(["Equilateral triangle", "Square", "Cube",])
+        self.SelectType.addItems(["Equilateral triangle", "Square", "Cube", "Sphere"])
         # fmt: on
 
         self.Side_Length = QDoubleSpinBox()
@@ -249,7 +369,7 @@ class AddObjectDialog(QDialog):
         self.Accept_Reject.accepted.connect(self.accept)
         self.Accept_Reject.rejected.connect(self.reject)
 
-    def get_data(self):
+    def get_data(self) -> ObjectDataType:
         return {
             "Type": self.SelectType.currentText(),
             "Side_Length": self.Side_Length.value(),
@@ -262,7 +382,7 @@ class AddObjectDialog(QDialog):
             "A": self.a,
         }
 
-    def show_color_dialog(self) -> list[float, float, float, float]:
+    def show_color_dialog(self) -> None:
         color = QColorDialog.getColor()
         if color.isValid():
             r, g, b, a = color.getRgb()
@@ -271,4 +391,6 @@ class AddObjectDialog(QDialog):
                 f"RGBA: r={self.r}, g={self.g}, b={self.b}, a={self.a}"
             )
 
-            return [self.r, self.g, self.b, self.a]
+            self.CurrentColor = (self.r, self.g, self.b, self.a)
+        else:
+            self.CurrentColor = (0.0, 0.0, 0.0, 1.0)

OpenGLWidget.py
@@ -1,44 +1,49 @@
+from typing import Any, Union
 from PyQt6.QtOpenGLWidgets import QOpenGLWidget
 from PyQt6.QtGui import QSurfaceFormat
-from PyQt6.QtOpenGL import QOpenGLVersionProfile, QOpenGLWindow
-from PyQt6.QtCore import QPoint, Qt, QModelIndex
+from PyQt6.QtCore import QPointF, Qt, QModelIndex
 from OpenGL.GLU import gluLookAt
 from OpenGL.GL import *
 import numpy as np
 import os
 import sys
 from numpy.typing import NDArray
+from utils import Physics_utils
 
 # IS_PERSPECTIVE = True                               # Perspective projection
 # VIEW = np.array([-0.8, 0.8, -0.8, 0.8, 1.0, 20.0])  # Visual body left/right/top/bottom/near/far six surface
 # SCALE_K = np.array([1.0, 1.0, 1.0])                 # Model scaling ratio
-# EYE = np.array([0.0, 0.0, 2.0])                     # The position of the eyes (default positive direction of the Z-axis)
-# LOOK_AT = np.array([0.0, 0.0, 0.0])                 # Reference point for aiming direction (default at the coordinate origin)
-# EYE_UP = np.array([0.0, 1.0, 0.0])                  # Define the top (default positive direction of the Y-axis) for the observer
+# EYE = np.array([0.0, 0.0, 2.0])                     # The position of the eyes (default positive Direction of the Z-axis)
+# LOOK_AT = np.array([0.0, 0.0, 0.0])                 # Reference point for aiming Direction (default at the coordinate origin)
+# EYE_UP = np.array([0.0, 1.0, 0.0])                  # Define the top (default positive Direction of the Y-axis) for the observer
 # WIN_W, WIN_H = 640, 480                             # Variables for saving the width and height of the window
 # LEFT_IS_DOWNED = False                              # The left mouse button was pressed
 # MOUSE_X, MOUSE_Y = 0, 0                             # The starting position saved when examining the mouse displacement
 
 
 class Simulator(QOpenGLWidget):
-    def __init__(self, window_ui, parent=None):
+    def __init__(self, window_ui, parent=None) -> None:
         super().__init__(parent)
 
         self.window_self = window_ui
         # Camera
-        self.EYE: NDArray = np.array([0.0, 0.0, 5.0])
-        self.LOOK_AT: NDArray = np.array([0.0, 0.0, 0.0])
-        self.EYE_UP: NDArray = np.array([0.0, 1.0, 0.0])
+        self.EYE: NDArray[np.floating[Any]] = np.array([0.0, 0.0, 5.0])
+        self.LOOK_AT: NDArray[np.floating[Any]] = np.array([0.0, 0.0, 0.0])
+        self.EYE_UP: NDArray[np.floating[Any]] = np.array([0.0, 1.0, 0.0])
 
-        self.DIST, self.PHI, self.THETA = self.get_posture()
-
-        self.SCALE_K: NDArray = np.array([1.0, 1.0, 1.0])
+        self.SCALE_K: NDArray[np.floating[Any]] = np.array([1.0, 1.0, 1.0])
         self.IS_PERSPECTIVE: bool = True
-        self.VIEW: NDArray = np.array([-0.8, 0.8, -0.8, 0.8, 1.0, 20.0])
+        self.COORDINATE_AXIS: bool = False
+        self.PLANE: bool = False
+        self.VIEW: NDArray[np.floating[Any]] = np.array(
+            [-0.8, 0.8, -0.8, 0.8, 1.0, 20.0]
+        )
 
-        self.LeftDown: bool = False
-        self.LastPos = QPoint()
+        self.RightDown: bool = False
+        self.LastPos: QPointF = QPointF()
         self.Graphics: list = []
+        self.Coordinate_Data = None
+        self.Plane_Data = None
 
     def initializeGL(self):
         self.fmt = QSurfaceFormat()
@@ -54,12 +59,12 @@ class Simulator(QOpenGLWidget):
         )  # Set the depth test function (GL_LEQUAL is just one of the options)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glUseProgram(self.shader_program)
-        # Set the projection matrix
 
     def paintGL(self):
         self.makeCurrent()
         w, h = self.width(), self.height()
 
+        # Set the projection matrix
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         if self.IS_PERSPECTIVE:
@@ -114,9 +119,7 @@ class Simulator(QOpenGLWidget):
         mvp_loc = glGetUniformLocation(self.shader_program, "mvp")
         glUniformMatrix4fv(mvp_loc, 1, GL_FALSE, mvp)
 
-        self.draw_coordinates()
-
-        for vao, ebo, index_len, object_type in self.Graphics:
+        for vao, _, _, index_len, object_type in self.Graphics:
             glBindVertexArray(vao)
             glDrawElements(object_type, index_len, GL_UNSIGNED_INT, ctypes.c_void_p(0))
             glBindVertexArray(0)
@@ -124,53 +127,128 @@ class Simulator(QOpenGLWidget):
         # Clear the buffer and send the instructions to the hardware for immediate execution
         glFlush()
 
-    def get_posture(self):
-        delta: NDArray = self.EYE - self.LOOK_AT
-        DIST = np.linalg.norm(delta)
-        if DIST < 0.001:
-            return 0.0, 0.0, 0.0
-
-        PHI = np.arcsin(delta[1] / DIST)
-        r = np.linalg.norm(delta[[0, 2]])
-        THETA = np.arcsin(delta[0] / r) if r > 0.001 else 0.0
-
-        return DIST, PHI, THETA
-
     def mousePressEvent(self, event):
-        if event.button() == Qt.MouseButton.LeftButton:
-            self.LeftDown = True
+        self.setFocus()
+        if event.button() == Qt.MouseButton.RightButton:
+            self.RightDown = True
             self.LastPos = event.position()
 
     def mouseMoveEvent(self, event):
-        if not self.LeftDown:
+        if not self.RightDown:
             return
 
-        dx = self.LastPos.x() - event.position().x()
-        dy = event.position().y() - self.LastPos.y()
-        self.LastPos = event.pos()
+        # Get the position of the mouse and its displacement
+        Current_Position = event.position()
+        dx = self.LastPos.x() - Current_Position.x()
+        dy = Current_Position.y() - self.LastPos.y()
+        self.LastPos = Current_Position
 
-        w, h = self.width(), self.height()
-        self.PHI += 2 * np.pi * dy / h
-        self.THETA += 2 * np.pi * dx / w
+        # Calculate the vector from the current camera to the target point
+        DirectionOfEye: NDArray[np.floating[Any]] = self.LOOK_AT - self.EYE
+        Distance: np.floating[Any] = np.linalg.norm(DirectionOfEye)
+        if Distance < 1e-6:
+            return
 
-        self.PHI %= 2 * np.pi
-        self.THETA %= 2 * np.pi
+        Theta: float = -dx * 0.002
+        Phi: float = -dy * 0.002
 
-        r = self.DIST * np.cos(self.PHI)
-        self.EYE[0] = self.LOOK_AT[0] + r * np.sin(self.THETA)
-        self.EYE[1] = self.LOOK_AT[1] + self.DIST * np.sin(self.PHI)
-        self.EYE[2] = self.LOOK_AT[2] + r * np.cos(self.THETA)
+        Phi = np.clip(Phi, -np.pi / 2.0 + 0.01, np.pi / 2.0 - 0.01)
 
-        if np.pi / 2 < self.PHI < 3 * np.pi / 2:
-            self.EYE_UP[1] = -1.0
+        # Calculate the position of the camera rotated
+        # Unit Vector
+        UnitVector: NDArray[np.floating[Any]] = DirectionOfEye / Distance
+        UP_Vector: NDArray[np.floating[Any]] = self.EYE_UP
+
+        # Right
+        Right: NDArray[np.floating[Any]] = np.cross(UnitVector, UP_Vector)
+        Right_norm: np.floating = np.linalg.norm(Right)
+        if Right_norm < 1e-6:
+            Right = np.array([1.0, 0.0, 0.0], dtype=np.float32)
         else:
-            self.EYE_UP[1] = 1.0
+            Right = Right / Right_norm
+
+        # Apply Vertical rotation
+        RotatedVerticalVector: NDArray[np.floating[Any]] = Physics_utils.rotate_vector(
+            UnitVector, UP_Vector, Theta
+        )
+
+        Right_2nd = np.cross(RotatedVerticalVector, UP_Vector)
+        Right_norm_2nd = np.linalg.norm(Right_2nd)
+        if Right_norm_2nd < 1e-6:
+            Right_2nd = np.array([1.0, 0.0, 0.0], dtype=np.float32)
+        else:
+            Right_2nd = Right_2nd / Right_norm_2nd
+
+        # Apply horizontal rotaion (The final vector)
+        FinalVector: NDArray[np.floating[Any]] = Physics_utils.rotate_vector(
+            RotatedVerticalVector, Right_2nd, Phi
+        )
+        FinalUP: NDArray[np.floating[Any]] = Physics_utils.rotate_vector(
+            UP_Vector, Right_2nd, Phi
+        )
+
+        # Update self.EYE, self.EYE_UP
+        self.EYE = self.LOOK_AT - FinalVector * Distance
+        self.EYE_UP = FinalUP / np.linalg.norm(FinalUP)
 
         self.update()
 
     def mouseReleaseEvent(self, event):
-        if event.button() == Qt.MouseButton.LeftButton:
-            self.LeftDown = False
+        if event.button() == Qt.MouseButton.RightButton:
+            self.RightDown = False
+
+    def keyPressEvent(self, event):
+        """
+        Handle keyboard key events and achieve camera position movement
+
+        Args:
+            event (QKeyEvent)
+        """
+
+        Step: float = 0.1
+
+        DirectionForward: NDArray[np.floating[Any]] = self.LOOK_AT - self.EYE
+        Norm_DirectionForward: np.floating = np.linalg.norm(DirectionForward)
+        if Norm_DirectionForward < 1e-6:
+            return
+        DirectionForward = DirectionForward / Norm_DirectionForward
+
+        DirectionRight: NDArray[np.floating[Any]] = np.cross(
+            DirectionForward, self.EYE_UP
+        )
+        Norm_DirectionRight: np.floating = np.linalg.norm(DirectionRight)
+        if Norm_DirectionRight < 1e-6:
+            DirectionRight = np.array([1.0, 0.0, 0.0], dtype=np.float32)
+        else:
+            DirectionRight = DirectionRight / Norm_DirectionRight
+
+        match event.key():
+
+            case Qt.Key.Key_W:
+                self.EYE += DirectionForward * Step
+                self.LOOK_AT += DirectionForward * Step
+
+            case Qt.Key.Key_S:
+                self.EYE -= DirectionForward * Step
+                self.LOOK_AT -= DirectionForward * Step
+
+            case Qt.Key.Key_A:
+                self.EYE -= DirectionRight * Step
+                self.LOOK_AT -= DirectionRight * Step
+
+            case Qt.Key.Key_D:
+                self.EYE += DirectionRight * Step
+                self.LOOK_AT += DirectionRight * Step
+
+            case Qt.Key.Key_Q:
+                self.EYE += self.EYE_UP * Step
+                self.LOOK_AT += self.EYE_UP * Step
+
+            case Qt.Key.Key_E:
+                self.EYE -= self.EYE_UP * Step
+                self.LOOK_AT -= self.EYE_UP * Step
+
+        self.update()
 
     def wheelEvent(self, event):
         delta = event.angleDelta().y()
@@ -186,6 +264,12 @@ class Simulator(QOpenGLWidget):
         self.update()
 
     def compile_shaders(self):
+        """
+        To use the shader
+
+        Returns:
+            shaders program
+        """
         # Vertex_shader
         vertex_shader = glCreateShader(GL_VERTEX_SHADER)
         glShaderSource(
@@ -211,7 +295,10 @@ class Simulator(QOpenGLWidget):
 
         return shader_program
 
-    def draw_coordinates(self):
+    def draw_coordinates(self) -> None:
+        """
+        To generate a coordinate axis on the screen
+        """
         self.makeCurrent()
 
         # fmt: off
@@ -237,17 +324,17 @@ class Simulator(QOpenGLWidget):
         # fmt: on
 
         # Vao
-        vao = glGenVertexArrays(1)
-        glBindVertexArray(vao)
+        self.AxisVao = glGenVertexArrays(1)
+        glBindVertexArray(self.AxisVao)
 
         # Vbo
-        vbo = glGenBuffers(1)
-        glBindBuffer(GL_ARRAY_BUFFER, vbo)
+        self.AxisVbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.AxisVbo)
         glBufferData(GL_ARRAY_BUFFER, Vertices.nbytes, Vertices, GL_STATIC_DRAW)
 
         # Ebo
-        ebo = glGenBuffers(1)
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
+        self.AxisEbo = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.AxisEbo)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, Indices.nbytes, Indices, GL_STATIC_DRAW)
 
         # Vertices
@@ -262,10 +349,26 @@ class Simulator(QOpenGLWidget):
         glBindBuffer(GL_ARRAY_BUFFER, 0)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
 
-        # self.Graphics.append((vao, ebo, len(indices), GL_LINES))
-        glBindVertexArray(vao)
-        glDrawElements(GL_LINES, len(Indices), GL_UNSIGNED_INT, ctypes.c_void_p(0))
-        glBindVertexArray(0)
+        index = len(self.Graphics)
+        self.Coordinate_Data = (
+            self.AxisVao,
+            self.AxisVbo,
+            self.AxisEbo,
+            len(Indices),
+            GL_LINES,
+        )
+        self.Graphics.append(
+            (self.AxisVao, self.AxisVbo, self.AxisEbo, len(Indices), GL_LINES)
+        )
+
+        self.window_self.ObjectList.beginInsertRows(QModelIndex(), index, index)
+        self.window_self.ObjectList.endInsertRows()
+
+        self.update()
+
+        # glBindVertexArray(vao)
+        # glDrawElements(GL_LINES, len(Indices), GL_UNSIGNED_INT, ctypes.c_void_p(0))
+        # glBindVertexArray(0)
         # # Start drawing world coordinates
         # glBegin(GL_LINES)
 
@@ -286,12 +389,91 @@ class Simulator(QOpenGLWidget):
 
         # glEnd()  # End
 
+    def draw_plane(self) -> None:
+        """
+        To generate a plane on the screen
+        """
+        self.makeCurrent()
+
+        # fmt: off
+        Vertices: NDArray[np.float32] = np.array([
+            -500, 10, -500, 2/3, 1/3, 0, 1.0,
+            500, 10, -500, 2/3, 1/3, 0, 1.0,
+            500, -10, -500, 2/3, 1/3, 0, 1.0,
+            -500, -10, -500, 2/3, 1/3, 0, 1.0,
+            -500, 10, 500, 2/3, 1/3, 0, 1.0,
+            500, 10, 500, 2/3, 1/3, 0, 1.0,
+            500, -10, 500, 2/3, 1/3, 0, 1.0,
+            -500, -10, 500, 2/3, 1/3, 0, 1.0,
+        ], dtype=np.float32)
+
+        Indices: NDArray[np.uint32] = np.array([
+            0, 1, 2, 3, # v0-v1-v2-v3
+            4, 5, 1, 0, # v4-v5-v1-v0
+            3, 2, 6, 7, # v3-v2-v6-v7
+            5, 4, 7, 6, # v5-v4-v7-v6
+            1, 5, 6, 2, # v1-v5-v6-v2
+            4, 0, 3, 7  # v4-v0-v3-v7
+        ], dtype=np.uint32)
+        # fmt: on
+
+        # Vao
+        self.PlaneVao = glGenVertexArrays(1)
+        glBindVertexArray(self.PlaneVao)
+
+        # Vbo
+        self.PlaneVbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.PlaneVbo)
+        glBufferData(GL_ARRAY_BUFFER, Vertices.nbytes, Vertices, GL_STATIC_DRAW)
+
+        # Ebo
+        self.PlaneEbo = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.PlaneEbo)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, Indices.nbytes, Indices, GL_STATIC_DRAW)
+
+        # Vertices
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * 4, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(0)
+
+        # Colors
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * 4, ctypes.c_void_p(3 * 4))
+        glEnableVertexAttribArray(1)
+
+        glBindVertexArray(0)
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
+
+        index = len(self.Graphics)
+        self.Plane_Data = (
+            self.PlaneVao,
+            self.PlaneVbo,
+            self.PlaneEbo,
+            len(Indices),
+            GL_QUADS,
+        )
+        self.Graphics.append(
+            (self.PlaneVao, self.PlaneVbo, self.PlaneEbo, len(Indices), GL_QUADS)
+        )
+
+        self.window_self.ObjectList.beginInsertRows(QModelIndex(), index, index)
+        self.window_self.ObjectList.endInsertRows()
+
+        self.update()
+
     def analysis_data(self, data: dict):
+        """
+        To analysis data were transfered from the Handler.py,
+        bind the vao, vbo, ebo,
+        and then send these info into self.Graphics to paint the object,
+
+        Args:
+            data (dict): the data of the object
+        """
         self.makeCurrent()
 
         ObjectType: int = data["Type"]
         Vertices: NDArray[np.float32] = data["Vertices"]
-        Indices: NDArray[np.float32] = data["Indices"]
+        Indices: NDArray[np.uint64] = data["Indices"]
 
         vao = glGenVertexArrays(1)
         glBindVertexArray(vao)
@@ -316,7 +498,7 @@ class Simulator(QOpenGLWidget):
 
         index = len(self.Graphics)
 
-        self.Graphics.append((vao, ebo, len(Indices), ObjectType))
+        self.Graphics.append((vao, vbo, ebo, len(Indices), ObjectType))
 
         self.window_self.ObjectList.beginInsertRows(QModelIndex(), index, index)
         self.window_self.ObjectList.endInsertRows()
@@ -324,10 +506,32 @@ class Simulator(QOpenGLWidget):
         self.update()
 
     def load_shader(self, file_path: str):
+        """_summary_
+        Load shader from file_path
+
+        Args:
+            file_path (str): the file path
+
+        Returns:
+            the shader
+        """
         if getattr(sys, "frozen", False):
-            BasePth = sys._MEIPASS
+            BasePth = sys._MEIPASS  # type: ignore[attr-defined]
         else:
             BasePth = os.path.abspath(".")
         dir = os.path.join(BasePth, file_path)
         with open(dir, "r") as f:
             return f.read()
+
+    def delete_single_object(self, vao, vbo, ebo) -> None:
+        """
+        To release the buffer of the object's vao, vbo, ebo
+
+        Args:
+            vao
+            vbo
+            ebo
+        """
+        glDeleteVertexArrays(1, [vao])
+        glDeleteBuffers(1, [vbo])
+        glDeleteBuffers(1, [ebo])